0a0e8cb3889940be4e668af8210b3554c84760c3
natia <90927569+natiasha@users.noreply.github.com>
2021-10-25 17:57:07 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const number=[1, 2, 3, 4, 5];
+let s=number[0]+number[1]+number[2]+number[3]+number[4];
+console.log(s);
+
+const person={Name: "Natia", Edge: "25", Address: "Tbilisi"};
+console.log('my Name is' + ' '+ person.Name);
+console.log('my Age is' + ' '+ person.Age + ' '+ 'Year old');
+console.log('my Address is' + ' '+ person.Address);
+ 
+function myEdge(Edge) {
+let=result;{
+if (Edge<19)
+result='I am Tynager';}
+{else
+ result='I am edult';}
+ console.log(result);}
